2d653d4809ca8cbb1f54d0c4f98978b00ab26774
carolinez21 <92185211+carolinez21@users.noreply.github.com>
2022-05-02 10:17:14 -0400

    delete chatbot widget

--- src/components/user_side/Footer.js
+++ src/components/user_side/Footer.js
@@ -2,7 +2,5 @@ import React from "react";
 import "../../style/user_side/Footer.css"
 
 export default function Footer(props) {
-  return <div className="user_side-footer">    
-        <script>const c="[COLOUR]"; const u="https://mat.massively.ai/chat?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnQiOiJrTFVJdGtKZUdKZ1B4T3JFejlJSzE2MTBybzQyIiwic3RvcnlJZCI6ImNlZDIwYTc3LTUxNTctNDg2Yy1iYjc1LWQzZDIzYThlODc4ZiIsImJvdElkIjoiMmFhZTk2M2QtZWI5MC00NTYyLTk1OGItNGZiNmUwYWJiNzFkIiwiaWF0IjoxNjUxMDg4MzM1fQ.13O67WG6wbHnZOwPuI_vi6mjI4XJ-h8Cnbnb6SlRY38";</script>
-        <script type="text/javascript" src="https://s3.amazonaws.com/uploads.massively.ai/widget.min.js" async defer></script></div>;
+  return <div className="user_side-footer"></div>;
 }
