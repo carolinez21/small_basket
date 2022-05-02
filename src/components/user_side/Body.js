3ba37428b1cb922d385b4ca91821fbf69a005233
carolinez21 <92185211+carolinez21@users.noreply.github.com>
2022-05-02 09:52:52 -0400

    test chat widget

--- src/components/user_side/Body.js
+++ src/components/user_side/Body.js
@@ -163,6 +163,10 @@ export default function Body(props) {
         </div>
         <button className="user_side-body-downlaod_button">Download</button>
       </div>
+        <!-- Chat widget -->
+        <script>const c="[COLOUR]"; const u="https://mat.massively.ai/chat?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnQiOiJrTFVJdGtKZUdKZ1B4T3JFejlJSzE2MTBybzQyIiwic3RvcnlJZCI6ImNlZDIwYTc3LTUxNTctNDg2Yy1iYjc1LWQzZDIzYThlODc4ZiIsImJvdElkIjoiMmFhZTk2M2QtZWI5MC00NTYyLTk1OGItNGZiNmUwYWJiNzFkIiwiaWF0IjoxNjUxMDg4MzM1fQ.13O67WG6wbHnZOwPuI_vi6mjI4XJ-h8Cnbnb6SlRY38";</script>
+        <script type="text/javascript" src="https://s3.amazonaws.com/uploads.massively.ai/widget.min.js" async defer></script>
+        
     </section>
   );
 }
